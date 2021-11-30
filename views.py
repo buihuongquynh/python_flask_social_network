072f72f115b83d9f4c16efa8b68102240b169621
Ford1508 <87651131+Ford1508@users.noreply.github.com>
2021-11-30 19:16:13 +0700

    update website

--- views.py
+++ views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from operator import le
+from flask import Blueprint, render_template, request
+from flask.helpers import flash
+from flask_login import login_user, login_required, logout_user, current_user
+from models import Post, User
+from initial import db
 
 views = Blueprint('views',__name__)
 
-@views.route('/')
+@views.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        post = request.form.get('post')
+        if len(post) < 1:
+            flash('post is too short!', category='error')
+        else:
+            new_post = Post(body = post, author_id = current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Post is created!', category='success')
+    return render_template('home.html', user= current_user, posts= current_user.posts)
