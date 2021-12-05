e6618cd3cbfb12efbf5abf1378b59db9abeff82f
Ford1508 <87651131+Ford1508@users.noreply.github.com>
2021-12-05 23:53:15 +0700

    add update, delete action. Fix home page.

--- models.py
+++ models.py
@@ -26,6 +26,7 @@ class Comment(db.Model):
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, nullable = True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
--- views.py
+++ views.py
@@ -1,9 +1,10 @@
-from operator import le
-from flask import Blueprint, render_template, request
-from flask.helpers import flash
+import os
+from flask import Blueprint, render_template, request, redirect
+from flask.helpers import flash, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 from models import Post, User
 from initial import db
+from werkzeug.utils import secure_filename  
 
 views = Blueprint('views',__name__)
 
@@ -12,14 +13,72 @@ def home():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     if request.method == 'POST':
         post = request.form.get('post')
+        pic = request.files['image']
+        picdata = ""
+        if not pic:
+            picdata = ""
+        else:
+            filename = secure_filename(pic.filename)
+            pic.save(os.path.join("D:\Python_File\python_flask_social_network\static",filename))
+            picdata = filename
         if len(post) < 1:
             flash('post is too short!', category='error')
         else:
-            new_post = Post(body = post, author_id = current_user.id)
+            new_post = Post(body = post, author_id = current_user.id, img = picdata)
             db.session.add(new_post)
             db.session.commit()
             flash('Post is created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template('home.html', user= current_user, posts= posts)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.author_id:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+
+    return redirect(url_for('views.home'))
+
+@views.route("/edit-post/<id>", methods = ['GET', 'POST'])
+@login_required
+def edit_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.author_id:
+        flash('You do not have permission to delete this post.', category='error')
+    elif request.method == 'POST':
+        body = request.form.get('post')
+        pic = request.files['image']
+        picdata = ""
+        if not pic:
+            picdata = ""
+        else:
+            filename = secure_filename(pic.filename)
+            pic.save(os.path.join("D:\Python_File\python_flask_social_network\static",filename))
+            picdata = filename
+        if len(body) < 1:
+            flash('post is too short!', category='error')
+        else:
+            # new_post = Post(body = body, author_id = current_user.id, img = picdata)
+            post.body = body
+            post.img = picdata
+            # db.session.add(new_post)
+            db.session.commit()
+            flash('Post is updated!', category='success')
+            return redirect(url_for('views.home'))
+    return render_template('edit-post.html', user= current_user, post= post)
+
+
 
 @views.errorhandler(404)
 def page_not_found(e):
