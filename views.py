b5c5c9c3ecfaca0bcbadf5d8523cfe12e42a2a73
Ford1508 <87651131+Ford1508@users.noreply.github.com>
2021-12-06 17:18:15 +0700

    add create and delete comment actions.

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, render_template, request, redirect
 from flask.helpers import flash, url_for
 from flask_login import login_user, login_required, logout_user, current_user
-from models import Post, User
+from models import Post, User, Comment
 from initial import db
 from werkzeug.utils import secure_filename  
 
@@ -78,6 +78,55 @@ def edit_post(id):
             return redirect(url_for('views.home'))
     return render_template('edit-post.html', user= current_user, post= post)
 
+
+@views.route("/posts/<author_id>")
+@login_required
+def posts(author_id):
+    user = User.query.filter_by(id=author_id).first()
+    username = user.username
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = Post.query.filter_by(author_id=user.id).all()
+    return render_template("userpost.html", user=current_user, posts=posts, username=username)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                content=text, user_id=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.user_id and current_user.id != comment.post_comment.author_id:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
 
 
 @views.errorhandler(404)
